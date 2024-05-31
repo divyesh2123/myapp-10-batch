c502ec995f0abe59d2cefece1a26eaad2e08329e
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-05-31 11:29:39 +0530

    changes for  the this

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -8,9 +8,10 @@ import Products from './Products.jsx'
 import Registration from './Registration.jsx'
 import Emp from './Emp.jsx'
 import Profile from './Profile.jsx'
+import FullFormFunction from './FullFormFunction.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile />
+    <FullFormFunction />
   </React.StrictMode>,
 )
